5eb48a12d6e0a858d761c64a0d3800ad08b8db1f
Jedrzej Ginter <jedrzej.ginter@monterail.com>
2019-06-19 12:59:24 +0200

    Add code coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,15 @@ module.exports = {
       diagnostics: false,
     },
   },
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 95,
+      functions: 95,
+      lines: 95,
+      statements: 95,
+    },
+  },
   roots: ["<rootDir>"],
   transform: {
     "^.+\\.tsx?$": "ts-jest",
