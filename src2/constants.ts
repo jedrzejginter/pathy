026fa6bc24becebbfd658f11edc5bb3cdea43dee
Jedrzej Ginter <jedrzej.ginter@monterail.com>
2019-06-21 11:27:53 +0200

    Add regular expressions for int and uint

--- src2/constants.ts
+++ src2/constants.ts
@@ -1,2 +1,4 @@
-export const BOOLEAN_REGEXP = /^(true|false)$/;
+export const BOOL_REGEXP = /^(true|false)$/;
 export const FLOAT_REGEXP = /^(0|0\.\d+|-?[1-9]\d*|-?0\.\d*[1-9]\d*|-?[1-9]\d*\.\d+)$/;
+export const INT_REGEXP = /^(0|-?[1-9]\d*)$/;
+export const UINT_REGEXP = /^(0|[1-9]\d*)$/;
--- tests2/constants.spec.ts
+++ tests2/constants.spec.ts
@@ -1,33 +1,33 @@
-import { BOOLEAN_REGEXP, FLOAT_REGEXP } from "../src2/constants";
+import { BOOL_REGEXP, FLOAT_REGEXP, INT_REGEXP, UINT_REGEXP } from "../src2/constants";
 
 describe("constants", () => {
-  describe("BOOLEAN_REGEXP", () => {
+  describe("BOOL_REGEXP", () => {
     it("should be a RegExp instance", () => {
-      expect(BOOLEAN_REGEXP).toBeInstanceOf(RegExp);
+      expect(BOOL_REGEXP).toBeInstanceOf(RegExp);
     });
 
     it("should have a starting ^", () => {
-      expect(BOOLEAN_REGEXP.source).toMatch(/^\^/);
+      expect(BOOL_REGEXP.source).toMatch(/^\^/);
     });
 
     it("should have an ending $", () => {
-      expect(BOOLEAN_REGEXP.source).toMatch(/\$$/);
+      expect(BOOL_REGEXP.source).toMatch(/\$$/);
     });
 
     it("should match 'true' string", () => {
-      expect(BOOLEAN_REGEXP.test("true")).toBe(true);
+      expect(BOOL_REGEXP.test("true")).toBe(true);
     });
 
     it("should match 'false' string", () => {
-      expect(BOOLEAN_REGEXP.test("false")).toBe(true);
+      expect(BOOL_REGEXP.test("false")).toBe(true);
     });
 
     it("should fail for string other than 'true' and 'false'", () => {
-      expect(BOOLEAN_REGEXP.test("")).toBe(false);
-      expect(BOOLEAN_REGEXP.test("1")).toBe(false);
-      expect(BOOLEAN_REGEXP.test("0")).toBe(false);
-      expect(BOOLEAN_REGEXP.test("yes")).toBe(false);
-      expect(BOOLEAN_REGEXP.test("no")).toBe(false);
+      expect(BOOL_REGEXP.test("")).toBe(false);
+      expect(BOOL_REGEXP.test("1")).toBe(false);
+      expect(BOOL_REGEXP.test("0")).toBe(false);
+      expect(BOOL_REGEXP.test("yes")).toBe(false);
+      expect(BOOL_REGEXP.test("no")).toBe(false);
     });
   });
 
@@ -56,6 +56,58 @@ describe("constants", () => {
       expect(FLOAT_REGEXP.test("-123")).toBe(true);
       expect(FLOAT_REGEXP.test("0")).toBe(true);
       expect(FLOAT_REGEXP.test("123")).toBe(true);
+    });
+  });
+
+  describe("INT_REGEXP", () => {
+    it("should be a RegExp instance", () => {
+      expect(INT_REGEXP).toBeInstanceOf(RegExp);
+    });
+
+    it("should have a starting ^", () => {
+      expect(INT_REGEXP.source).toMatch(/^\^/);
+    });
+
+    it("should have an ending $", () => {
+      expect(INT_REGEXP.source).toMatch(/\$$/);
+    });
+
+    it("should match integer", () => {
+      expect(INT_REGEXP.test("-123")).toBe(true);
+      expect(INT_REGEXP.test("0")).toBe(true);
+      expect(INT_REGEXP.test("123")).toBe(true);
+    });
+
+    it("should not match -0", () => {
+      expect(INT_REGEXP.test("-0")).toBe(false);
+    });
+  });
+
+  describe("UINT_REGEXP", () => {
+    it("should be a RegExp instance", () => {
+      expect(UINT_REGEXP).toBeInstanceOf(RegExp);
+    });
+
+    it("should have a starting ^", () => {
+      expect(UINT_REGEXP.source).toMatch(/^\^/);
+    });
+
+    it("should have an ending $", () => {
+      expect(UINT_REGEXP.source).toMatch(/\$$/);
+    });
+
+    it("should match positive integer", () => {
+      expect(UINT_REGEXP.test("0")).toBe(true);
+      expect(UINT_REGEXP.test("123")).toBe(true);
+    });
+
+    it("should not match -0", () => {
+      expect(UINT_REGEXP.test("-0")).toBe(false);
+    });
+
+    it("should not match negative integer", () => {
+      expect(UINT_REGEXP.test("-1")).toBe(false);
+      expect(UINT_REGEXP.test("-123")).toBe(false);
     });
   });
 });
