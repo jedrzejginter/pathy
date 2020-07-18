5cf9102e0b62d09718c7315b8c653b7d56de0a6e
Jedrzej Ginter <jedrzej.ginter@monterail.com>
2020-07-18 19:42:16 +0200

    New regexes

--- src2/constants.ts
+++ src2/constants.ts
@@ -1,4 +1,6 @@
 export const BOOL_REGEXP = /^(true|false)$/;
 export const FLOAT_REGEXP = /^(0|0\.\d+|-?[1-9]\d*|-?0\.\d*[1-9]\d*|-?[1-9]\d*\.\d+)$/;
 export const INT_REGEXP = /^(0|-?[1-9]\d*)$/;
+export const STR_REGEXP = /([^\/]+)/;
 export const UINT_REGEXP = /^(0|[1-9]\d*)$/;
+export const UUID_REGEXP = /([\da-f]{8}-[\da-f]{4}-[\da-f]{4}-[\da-f]{4}-[\da-f]{12})/;
